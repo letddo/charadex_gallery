15da8307b79905abcf714b7e0fe4156c29b35a79
M4CH1N4097 <jabberwocks134@gmail.com>
2025-11-07 00:13:36 +0900

    Update and rename gallery.js to loggallery.js

--- styles/js/pages/loggallery.js
+++ styles/js/pages/loggallery.js
@@ -11,17 +11,17 @@ document.addEventListener("DOMContentLoaded", async () => {
 
   let dex = await charadex.initialize.page(
     null,
-    charadex.page.gallery,
+    charadex.page.loggallery,
     null, 
     async (listData) => {
 
-      if (listData.type == 'gallery') {
+      if (listData.type == 'loggallery') {
 
         // Create the log dex
-        if (charadex.tools.checkArray(listData.galleryArray[0]['갤러리내역'])) {
+        if (charadex.tools.checkArray(listData.loggalleryArray[0]['갤러리내역'])) {
           let logs = await charadex.initialize.page(
-            listData.galleryArray[0]['갤러리내역'],
-            charadex.page.gallery.relatedData['갤러리 내역']
+            listData.loggalleryArray[0]['갤러리내역'],
+            charadex.page.loggallery.relatedData['갤러리 내역']
           );
         }
 
