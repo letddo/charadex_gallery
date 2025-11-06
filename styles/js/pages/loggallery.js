3c264922458f3078b4e61ce8b910d45cc6a7f047
M4CH1N4097 <jabberwocks134@gmail.com>
2025-11-07 01:49:34 +0900

    Update loggallery.js

--- styles/js/pages/loggallery.js
+++ styles/js/pages/loggallery.js
@@ -21,6 +21,6 @@ document.addEventListener("DOMContentLoaded", async () => {
         if (charadex.tools.checkArray(listData.loggalleryArray[0]['갤러리내역'])) {
           let logs = await charadex.initialize.page(
             listData.loggalleryArray[0]['갤러리내역'],
-            charadex.page.loggallery.relatedData['갤러리 내역']
+            charadex.page.loggallery.relatedData['갤러리내역']
           );
         }
